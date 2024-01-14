ecd8c13b20bff7743064e7ceb57388d78a8cf678
Rizwan462 <139914755+Rizwan462@users.noreply.github.com>
2024-01-14 16:01:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	if(str.length === 0){
+		return "null";
+	}
+	let charcont = {};
+	for(let char of str){
+		charcount[char] = (charcont[char] || 0)+1;
+	}
+	for(let char of str){
+		if(charcont[char] === 1){
+			return char;
+		}
+	}
+	return "null";
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
+console.log(result);
